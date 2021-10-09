df6aa6acda7ceaa9a10a61ca46e00b365dcb63ea
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-10-09 16:15:52 +0900

    fix factory bot

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,9 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do # describe: definition of testing group
   it 'tests article object' do
     # article = Article.create({title: 'sample article', content: 'Sample content'})
-    # expect(1).to be_positive # be_positive => 1.positive? == true
-    # expect(5).to be > 3
-    article = FactoryBot.create(:article)
+    article = create(:article)
     expect(article.title).to eq("Sample article")
   end
 end
