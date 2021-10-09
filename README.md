2b536eecc1b9263d4195a52b6baa52ca0451549f
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-10-09 19:27:48 +0900

    wip chapter27

--- README.md
+++ README.md
@@ -2,4 +2,23 @@
 Rails x Docker環境構築
 https://zenn.dev/tmasuyama1114/articles/rails-docker-6x-how-to
 
-webpackerはDockerfileとコマンドから削除
+- webpackerはDockerfileとコマンドから削除
+
+
+
+Rspecの実行
+
+```
+rspec spec/routing/articles_spec.rb  --format documentation
+```
+
+binding.pry
+
+```
+class Hoge
+
+~~~
+binding.pry
+
+
+```
---
+++ spec/requests/articles_spec.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe ArticlesController do
+  describe '#index' do
+    it 'returns a success resopnse' do
+      get '/articles'
+      # expect(response.status).to eq(200)
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'returns a proper JSON' do
+      article = create :article
+      get '/articles'
+
+      expect(json_data).to eq(
+        [
+          {
+            id: article.id.to_s,
+            type: 'article',
+            attributes: {
+              title: article.title,
+              content: article.content,
+              slug: article.slug
+            }
+          }
+        ]
+      )
+    end
+  end
+end
