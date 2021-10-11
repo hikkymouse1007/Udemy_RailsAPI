31a11b175b65828ac483073c72f4fb2d1d0cdb9c
hikkymouse1007 <mc.mdt1007it@gmail.com>
2021-10-11 12:38:50 +0900

    wip chapter 30

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'mysql2', '>= 0.4.4'
 gem 'puma', '~> 4.1'
 gem 'pry-rails'
 gem 'jsonapi-serializer'
+gem 'jsom-pagination'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.7'
 # Use Redis adapter to run Action Cable in production
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,25 @@
 class ArticlesController < ApplicationController
   def index
     articles = Article.recent
+    paginated = paginator.call(
+      articles,
+      params: pagination_params,
+      base_url: request.url
+    )
+    binding.pry
+    
     render json: serializer.new(articles), status: :ok
   end
 
   def serializer
     ArticleSerializer
+  end
+
+  def paginator
+    JSOM::Pagination::Paginator.new
+  end
+
+  def pagination_params
+    params.permit![:page]
   end
 end
